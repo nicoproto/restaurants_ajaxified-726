b1cbe7d351d8d9da6cd6ce34328c27bdeea52f4a
Nico Proto <nicolasproto@gmail.com>
2021-11-19 11:21:28 +0100

    Finished lecture

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,15 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id]) # comes from the route params
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render 'restaurants/show'
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.json # Follow the classic Rails flow and look for a create.json view
+      else
+        format.html { render 'restaurants/show' }
+        format.json # Follow the classic Rails flow and look for a create.json view
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: [ :index, :show ] do
+  resources :restaurants, only: [ :index, :show, :destroy ] do
     resources :reviews, only: [:create]
   end
+
+  # resources :reviews, only: [:destroy]
 end
